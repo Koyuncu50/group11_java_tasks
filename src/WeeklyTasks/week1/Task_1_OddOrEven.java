fc95495c0dccef5555ff4962621e3314c2aea4e7
DavidCydeo <david@cydeo.com>
2025-01-07 23:09:34 +0300

    official Task1 added

---
+++ src/WeeklyTasks/week1/Task_1_OddOrEven.java
@@ -0,0 +1,13 @@
+package WeeklyTasks.week1;
+
+public class Task_1_OddOrEven {
+    public static void main(String[] args) {
+        //Write  a method which can identifies given number is even or odd
+
+       // identifyOddEven(5);
+       // identifyOddEven(6);
+
+
+    }
+
+}
