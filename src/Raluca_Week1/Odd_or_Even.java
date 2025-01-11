dc117a554f218c33855f55e1c21f9c5ec512e44a
Raluca Ilut <r.ilut@yahoo.com>
2025-01-11 14:53:56 -0600

    Task1 Solved

--- src/Raluca_Week1/Odd_or_Even.java
+++ src/Raluca_Week1/Odd_or_Even.java
@@ -2,7 +2,32 @@ package Raluca_Week1;
 
 public class Odd_or_Even {
     public static void main(String[] args) {
-        //My Solution
+    //test method
+        oddOrEven(5);
+        oddOrEven(6);
+
+        int a = 1003;
+        oddOrEven(a);
 
     }
+    //create method
+    public static void oddOrEven(int number){
+
+        if(number % 2 == 0){
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd number");
+        }
+
+    }
+
 }
+/*
+Numbers -- odd & even
+Write a method that can identify  a given number is even or odd.
+
+EX:
+       identify(5) --> "Odd"
+       identify(6) --> "Even"
+
+ */
