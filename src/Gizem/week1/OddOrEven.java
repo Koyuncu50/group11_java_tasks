1c3f1cb3c07f90df8656b1cdab9032e12bef9871
gizemyigittelli <gzmmtely@gmail.com>
2025-01-14 14:14:54 -0500

    Task 1.2 solved and updated

--- src/Gizem/week1/OddOrEven.java
+++ src/Gizem/week1/OddOrEven.java
@@ -10,6 +10,7 @@ public class OddOrEven {
 
     public static void identifyOddOrEven(int number) {
         if (number % 2 == 0) {
+            //smth
             System.out.println(number + "is odd number");
         }else {
             System.out.println(number + "is even number");
