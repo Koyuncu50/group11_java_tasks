ae18e53c58318898708c2309ca850f9cea382659
gizemyigittelli <gzmmtely@gmail.com>
2025-01-14 12:00:34 -0500

    Task 1 solved.

--- src/Gizem/week1/OddOrEven.java
+++ src/Gizem/week1/OddOrEven.java
@@ -3,6 +3,20 @@ package Gizem.week1;
 public class OddOrEven {
 
     public static void main(String[] args) {
-        System.out.println("Solution is here");
+
+        //identifyOddOrEven(3)
+        //identifyOddOrEven(4)
+    }
+
+    public static void identifyOddOrEven(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + "is odd number");
+        }else {
+            System.out.println(number + "is even number");
+        }
+
+
     }
 }
+
+
