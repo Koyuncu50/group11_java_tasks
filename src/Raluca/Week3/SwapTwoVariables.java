98efb58eed1c2af80715994a1b1ddd37b49a7af9
ilutral <r.ilut@yahoo.com>
2025-01-21 12:16:26 -0600

    Task 3 added and moved task 1 to new folder

---
+++ src/Raluca/Week3/SwapTwoVariables.java
@@ -0,0 +1,18 @@
+package Raluca.Week3;
+
+public class SwapTwoVariables {
+
+        public static void main(String[] args) {
+
+            // Create a method that swaps the values of 2 variables, without using a third variable
+
+
+
+
+
+
+        }
+
+    }
+
+
