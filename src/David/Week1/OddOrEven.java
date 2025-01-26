f3d50d9fa345f8cd7494700c74f77b30ab659463
aloer <aloergun99@gmail.com>
2025-01-26 11:00:22 -0500

    week3 test solved

--- src/David/Week1/OddOrEven.java
+++ src/David/Week1/OddOrEven.java
@@ -2,9 +2,8 @@ package David.Week1;
 
 public class OddOrEven {
     public static void main(String[] args) {
-
-
         // solution here
+
 
     }
 
---
+++ src/ergun/week03/swappingTwoValues.java
@@ -0,0 +1,20 @@
+package ergun.week03;
+
+public class swappingTwoValues {
+
+
+    public static void main(String[] args) {
+    int a = 3;
+    int b = 7;
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    System.out.println(a +" is new the value of a");
+    System.out.println(b+" is new the value of b");
+
+
+
+
+    }
+
+}
