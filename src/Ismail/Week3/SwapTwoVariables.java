10e5b26f96c00679cbbb9ebccb3b63eb3a6f08f9
smail <smail.aitahmed66@gmail.com>
2025-01-29 23:14:38 -0500

    Task 3 solved

---
+++ src/Ismail/Week3/SwapTwoVariables.java
@@ -0,0 +1,18 @@
+package Ismail.Week3;
+
+public class SwapTwoVariables {
+    public static void main(String[] args) {
+
+        SwapTwoNum(10,5);
+
+    }
+
+    public static void SwapTwoNum(int a, int b) {
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
