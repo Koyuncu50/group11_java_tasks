8f1d8401e6d3864cfcaf969b184b19e1593101c2
smail <smail.aitahmed66@gmail.com>
2025-02-11 02:38:44 -0500

    Week5 solved

--- src/Ismail/Week5/FrequencyOfCharacter.java
+++ src/Ismail/Week5/FrequencyOfCharacter.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class FrequencyOfCharacter {
 
-    public static void main(String[] args) {
- /*Write a return method that can find the frequency of characters
+    /*Write a return method that can find the frequency of characters
  Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
  */
+    public static void main(String[] args) {
+
         String str= "AAABBCDD";
         System.out.println("frequency Of Characters: " + frequencyOfChars(str));
 
