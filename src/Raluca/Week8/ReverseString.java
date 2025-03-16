1db83441580eadf777608cf13ad3f5025b7e50e4
Raluca Ilut <r.ilut@yahoo.com>
2025-03-16 14:35:07 -0500

    Task 8 Solved

---
+++ src/Raluca/Week8/ReverseString.java
@@ -0,0 +1,23 @@
+package Raluca.Week8;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println(reverseString("ABCD"));
+        System.out.println(reverseString("a bcdef"));
+
+
+    }
+
+    public static String reverseString(String str) {
+        String result= ""; // empty String variable
+        for (int i = str.length()-1; i >= 0 ; i--) {//ABCD -- starts from last letter (D)
+            result += str.charAt(i); // stores each letter in result
+        }
+        return result;
+    }
+
+}
+/*
+Write a return method that can reverse a String
+Ex: Reverse("ABCD"); ==> DCBA
+ */
